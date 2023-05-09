aa2f85de2f5b8973fede703ab4635bd67cad371c
YuJin Kwon <tokyj515@naver.com>
2023-05-09 22:15:51 +0900

    23.05.09

---
+++ src/baekjoon/bj10950.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class bj10950 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a + b);
+        }
+
+    }
+}
