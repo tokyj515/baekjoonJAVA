7be346a5a6943b5fe0e3391a0fcc111d5ed4ec3d
YuJin Kwon <tokyj515@naver.com>
2023-05-22 21:23:08 +0900

    23.05.22

---
+++ src/baekjoon/bj2743.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class bj2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+        sc.close();
+
+        System.out.println(s.length());
+    }
+}
